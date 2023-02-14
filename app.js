786506c5ace24e8b208176cf56744b74830771f2
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-02-14 07:44:29 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+
+import "text-encoding-polyfill";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import PolyfillCrypto from "react-native-webview-crypto";
+import Root from "./Root";
+import { injectStore } from "./utils/nostrV2/Event";
+import AppStateChecker from "./components/AppStateChecker";
+
+const App = () => {
+    injectStore(store);
+    return (
+        <Provider store={store}>
+            <AppStateChecker/>
+            <PolyfillCrypto />
+            <Root />
+        </Provider>
+    );
+};
+
+export default App;
