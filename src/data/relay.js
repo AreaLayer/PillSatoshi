6b6954b8673a1421582f96a7b57854aa2773cf2e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-03 15:19:25 -0300

    Update relay.js

--- src/data/relay.js
+++ src/data/relay.js
@@ -3,7 +3,8 @@ const require = require('relay');
 const kind = require('kind0');
 
 const relay = {
-  relay: wss//relay.damus.io;
+  relay: wss://relay.damus.io;
+  relay: wss://arealayer.relay.com;
 
 const npub
 
